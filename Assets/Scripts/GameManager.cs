3911cb3c9e963fd4145bb6181c1b8c2526cadc05
Amanda <amandaduarte2012@gmail.com>
2020-10-04 23:28:08 -0300

    Added Scene Management to Play bt on the Main Menus scene
    Added Main Menus and Vini_3D scenes to the Proj Settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-   
+
+    public string gameScene;
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
