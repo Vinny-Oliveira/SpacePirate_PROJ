1cd366604c6a982b809eba5a07fab24a738bfd3d
Vinicius Oliveira <vr.sena@gmail.com>
2020-10-08 14:34:43 -0400

    Win and Lose panels added and working for the thief

---
+++ Assets/Scripts/GameManagement/GameQuitter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Quit the application
+/// </summary>
+public class GameQuitter : MonoBehaviour {
+    
+    public void QuitGame() {
+        Debug.Log("Quitting the game");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/GameManagement/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manage how scenes are changed
+/// </summary>
+public class SceneChanger : MonoBehaviour {
+
+    public SceneField nextScene;
+
+    /// <summary>
+    /// Load a scene
+    /// </summary>
+    public void LoadScene() {
+        SceneManager.LoadScene(nextScene);
+    }
+
+}
