68ecc9e4db8e5671762e50b756f2f1ae859489bf
Amanda <amandaduarte2012@gmail.com>
2020-10-18 21:47:34 -0300

    Added Vol Control script and set it up with the game Audio Mixers
    + Set up the Level Song with the Canvas vol. controls
    + Added a Master Mixer Slider to replace the Mute Toogle (for now),
    + Changed the Exit Game Btn to a Main Menu one.

---
+++ Assets/Scripts/SoundControl/VolumeControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControls : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetMasterVol(float masterLvl)
+    {
+        masterMixer.SetFloat("volMaster", masterLvl);
+    }
+
+    public void SetSfxLv(float sfxLvl)
+    {
+        masterMixer.SetFloat("volSFXs", sfxLvl);
+    }
+
+    public void SetMusicLv(float musicLvl)
+    {
+        masterMixer.SetFloat("volSDTs", musicLvl);
+    }
+
+    public void SetVoiceContLv(float voiceContLvl)
+    {
+        masterMixer.SetFloat("voiceContVol", voiceContLvl);
+    }
+
+}
