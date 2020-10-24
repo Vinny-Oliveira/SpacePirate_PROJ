636ba9bc0b3f61f9324f994b3ec5964e752a9d60
Vinicius Oliveira <vr.sena@gmail.com>
2020-10-24 17:01:43 -0400

    Coded position of cameras and created the an Enemy script

--- Assets/Scripts/Characters/SecurityCamera.cs
+++ Assets/Scripts/Characters/SecurityCamera.cs
@@ -1,9 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class SecurityCamera : Character {
+public enum ECameraPosition { 
+    LEFT = -1,
+    CENTER = 0,
+    RIGHT = 1
+}
 
-    
+public class SecurityCamera : Enemy {
+
+    public ECameraPosition camPosition;
+    bool isForward;
+
+    /* Coordinates of fields of view */
+    public List<Vector2> leftCoords = new List<Vector2>();
+    public List<Vector2> centerCoords = new List<Vector2>();
+    public List<Vector2> rightCoords = new List<Vector2>();
+
+    /* Map each position enum to a list of coordinates */
+    Dictionary<ECameraPosition, List<Vector2>> dicCoords;
+
+    /// <summary>
+    /// Calculate the next camera position
+    /// </summary>
+    public void NextPosition() {
+        // Bounce back if maximum or minimum positions reached
+        if (camPosition == System.Enum.GetValues(typeof(ECameraPosition)).Cast<ECameraPosition>().Max()) {
+            camPosition = System.Enum.GetValues(typeof(ECameraPosition)).Cast<ECameraPosition>().Max() - 1;
+            isForward = false;
+        
+        } else if (camPosition == System.Enum.GetValues(typeof(ECameraPosition)).Cast<ECameraPosition>().Min()) {
+            camPosition = System.Enum.GetValues(typeof(ECameraPosition)).Cast<ECameraPosition>().Min() + 1;
+            isForward = true;
+        
+        // Simply go forward or backwards
+        } else {
+            camPosition = (isForward) ? (camPosition + 1) : (camPosition - 1);
+        }
+
+    }
+
+
 
 }
