18d71b3a05b55bfb3e4805de7ca8bcb5d24780d7
Vinicius Oliveira <vr.sena@gmail.com>
2020-11-10 21:40:13 -0500

    Set up the code structure for the cheat system

---
+++ Assets/Scripts/GameManagement/CheatManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatManager : MonoBehaviour {
+
+    public Dictionary<string, System.Action> cheatFunctions = new Dictionary<string, System.Action> { 
+        { "VC7930", TurnVisionConesOff },
+        { "DM3516", DoubleThiefsMoves },
+        { "KC5432", CollectEveryKeycard }
+    };
+
+    public void ActivateCheat(string code) { 
+        if (cheatFunctions.ContainsKey(code)) { 
+            
+        }
+    }
+
+    static void TurnVisionConesOff() {
+
+    }
+    
+    static void DoubleThiefsMoves() {
+
+    }
+    
+    static void CollectEveryKeycard() {
+
+    }
+
+}
