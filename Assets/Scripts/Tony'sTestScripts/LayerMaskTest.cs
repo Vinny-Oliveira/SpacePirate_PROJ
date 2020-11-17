998c1900f2f3cf4dd45ed7287fb85d04fe1667d4
TStgerm <T_stgermain@hotmail.com>
2020-11-16 22:02:37 -0500

    Added Ignore Raycast to custom renderer

--- Assets/Scripts/Tony'sTestScripts/LayerMaskTest.cs
+++ Assets/Scripts/Tony'sTestScripts/LayerMaskTest.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class LayerMaskTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera myCamera;
+
+    public int characterMask = ~(1<<8);
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit ,Mathf.Infinity, characterMask))
+        {
+            Debug.Log("Hit: " + hit.collider.gameObject.name);
+
+            // Do something with the object that was hit by the raycast.
+        }
     }
+
+    private void OnMouseOver()
+    {
+        if(LayerMask.GetMask() == 1<<8)
+        {
+            Debug.Log("This is the character");
+        }
+    }
+
 }
