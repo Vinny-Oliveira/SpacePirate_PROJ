79fae773d22de156d28eb1bcb7c0ac8f459d7287
TStgerm <T_stgermain@hotmail.com>
2020-11-22 17:23:29 -0500

    Created script for warp particles

---
+++ Assets/Scripts/TESTS/TestPaticles.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
+
+public class TestPaticles : MonoBehaviour
+{
+
+    public ParticleSystem exitParticle;
+    public ParticleSystem enterParticle;
+    public GameObject pirate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    [ContextMenu("ExitParticle")]
+    public void ExitParticle()
+    {
+        exitParticle.Play();
+        StartCoroutine("WaitForExit");
+    }
+
+    [ContextMenu("EnterParticle")]
+    public void EnterParticle()
+    {
+        enterParticle.Play();
+        StartCoroutine("WaitForEnter");
+    }
+
+    IEnumerator WaitForExit()
+    {
+        yield return new WaitForSeconds(1);
+        pirate.SetActive(false);
+    }
+
+    IEnumerator WaitForEnter()
+    {
+        yield return new WaitForSeconds(1);
+        pirate.SetActive(true);
+    }
+}
