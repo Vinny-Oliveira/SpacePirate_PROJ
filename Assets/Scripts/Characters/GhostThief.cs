711da894cc0d68a4a9b7383f56abae7811b419fa
Vinicius Oliveira <vr.sena@gmail.com>
2020-11-29 18:36:17 -0500

    Prepared script for ghost animation

---
+++ Assets/Scripts/Characters/GhostThief.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostThief : MonoBehaviour {
+
+    public GameObject ghost;
+    public Animator animator;
+    const string WALK_ANIM_NAME = "IsWalking";
+    const string GRAB_ANIM_NAME = "Grab";
+
+    /// <summary>
+    /// Play the ghost's walk animation
+    /// </summary>
+    public void PlayWalkAnimation() { 
+        if (animator) {
+            animator.SetBool(WALK_ANIM_NAME, true);
+        }
+    }
+    
+    /// <summary>
+    /// Play the ghost's grab animation
+    /// </summary>
+    public void PlayGrabAnimation() { 
+        if (animator) {
+            animator.SetTrigger(GRAB_ANIM_NAME);
+        }
+    }
+
+}
