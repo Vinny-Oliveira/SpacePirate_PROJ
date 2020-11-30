6f0b47fd689f19fad2c20d7e3b410f0b46d9582f
Vinicius Oliveira <45839644+Vinny-Oliveira@users.noreply.github.com>
2020-11-30 15:35:00 -0500

    Started script for error panels

---
+++ Assets/Scripts/Multiplayer/ErrorDialogHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorDialogHandler : MonoBehaviour {
+
+    public GameObject errorPanel;
+
+    public GameObject errorText1;
+    public GameObject errorText2;
+    public GameObject errorText3;
+
+    void TurnOnPanel(GameObject error_text) {
+        errorPanel.SetActive(true);
+        error_text.SetActive(true);
+    }
+
+    public void TurnOnPanel_1() {
+        TurnOnPanel(errorText1);
+    }
+    
+    public void TurnOnPanel_2() {
+        TurnOnPanel(errorText2);
+    }
+    
+    public void TurnOnPanel_3() {
+        TurnOnPanel(errorText3);
+    }
+
+}
