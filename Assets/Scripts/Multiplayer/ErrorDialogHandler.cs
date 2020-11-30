cb834e25a021aad4519c88aeb5ee3c39ff28abdb
Vinicius Oliveira <45839644+Vinny-Oliveira@users.noreply.github.com>
2020-11-30 17:14:13 -0500

    Prepared 4 variables for error messages

--- Assets/Scripts/Multiplayer/ErrorDialogHandler.cs
+++ Assets/Scripts/Multiplayer/ErrorDialogHandler.cs
@@ -6,25 +6,39 @@ public class ErrorDialogHandler : MonoBehaviour {
 
     public GameObject errorPanel;
 
-    public GameObject errorText1;
-    public GameObject errorText2;
-    public GameObject errorText3;
+    public GameObject errorText_NoPlayerName;
+    public GameObject errorText_NoRoomCreated;
+    public GameObject errorText_NotEnoughPlayers;
+    public GameObject errorText_WrongRoom;
 
+    /// <summary>
+    /// Turn off all messages and then turn on the one you need
+    /// </summary>
+    /// <param name="error_text"></param>
     void TurnOnPanel(GameObject error_text) {
+        errorText_NoPlayerName.SetActive(false);
+        errorText_NoRoomCreated.SetActive(false);
+        errorText_NotEnoughPlayers.SetActive(false);
+        errorText_WrongRoom.SetActive(false);
+
         errorPanel.SetActive(true);
         error_text.SetActive(true);
     }
 
-    public void TurnOnPanel_1() {
-        TurnOnPanel(errorText1);
+    public void TurnOnPanel_NoPlayerName() {
+        TurnOnPanel(errorText_NoPlayerName);
     }
     
-    public void TurnOnPanel_2() {
-        TurnOnPanel(errorText2);
+    public void TurnOnPanel_NoRoomCreated() {
+        TurnOnPanel(errorText_NoRoomCreated);
     }
     
-    public void TurnOnPanel_3() {
-        TurnOnPanel(errorText3);
+    public void TurnOnPanel_NotEnoughPlayers() {
+        TurnOnPanel(errorText_NotEnoughPlayers);
+    }
+    
+    public void TurnOnPanel_WrongRoom() {
+        TurnOnPanel(errorText_WrongRoom);
     }
 
 }
