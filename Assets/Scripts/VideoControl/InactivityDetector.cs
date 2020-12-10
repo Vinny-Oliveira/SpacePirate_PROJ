87aa00cf9b64e473cd94b20a643db920af9eeffa
Vinicius Oliveira <45839644+Vinny-Oliveira@users.noreply.github.com>
2020-12-10 11:10:58 -0500

    Inactivity detector added to main menu scene

---
+++ Assets/Scripts/VideoControl/InactivityDetector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class InactivityDetector : VideoController {
+
+    float fltTimeCounter;
+
+    protected override void Start() {
+        fltTimeCounter = 0f;
+        base.Start();
+        StartCoroutine(CountTimeOnScreen());
+    }
+
+    /// <summary>
+    /// Play the end video event if the user was inactive for a time greater than the length of the video.
+    /// Event: load the intro scene
+    /// </summary>
+    /// <param name="player"></param>
+    protected override void OnVideoEndReached(VideoPlayer player) {
+        if (fltTimeCounter > videoPlayer.length - 2f) { 
+            base.OnVideoEndReached(player);
+        }
+    }
+
+    /// <summary>
+    /// Count every second the user spend on this scene
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CountTimeOnScreen() {
+        StartCoroutine(Wait_For_Input());
+        yield return new WaitForSeconds(1f);
+        fltTimeCounter++;
+        StartCoroutine(CountTimeOnScreen());
+    }
+
+    /// <summary>
+    /// Zero out the counter whenever the user inputs something
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Wait_For_Input() {
+        yield return new WaitUntil(() => Input.anyKeyDown);
+        fltTimeCounter = 0f;
+        StartCoroutine(Wait_For_Input());
+    }
+
+}
--- Assets/Scripts/VideoControl/VideoController.cs
+++ Assets/Scripts/VideoControl/VideoController.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroVideoController : MonoBehaviour
-{
+public class VideoController : MonoBehaviour {
+
     public UnityEngine.Video.VideoPlayer videoPlayer;
     public SceneChanger sceneChanger;
 
-    private void Start() {
+    protected virtual void Start() {
         // Add the event for the end of the video
         videoPlayer.loopPointReached += OnVideoEndReached;
     }
 
     /// <summary>
-    /// Event for when the video has reached ots end: change
+    /// Event for when the video has reached its end: load the main menu scene
     /// </summary>
     /// <param name="player"></param>
-    void OnVideoEndReached(UnityEngine.Video.VideoPlayer player) {
+    protected virtual void OnVideoEndReached(UnityEngine.Video.VideoPlayer player) {
         sceneChanger.LoadScene();
     }
 
